68dc4b0738731b31ef84fbaff464cc02eeb50491
MiniPanado <lucasdarochacerqueira18@gmail.com>
2026-09-02 02:12:54 +0100

    printf

--- printf/srcs/ft_parse_flags_bonus.c
+++ printf/srcs/ft_parse_flags_bonus.c
@@ -2,6 +2,43 @@
 
 int	ft_parse_flags(const char **format, t_flags *flags)
 {
+	flags->minus = 0;
+	flags->zero = 0;
+	flags->hash = 0;
+	flags->plus = 0;
+	flags->space = 0;
+	flags->width = 0;
+	flags->precision = 0;
+	flags->has_precision = 0;
+	while (**format == '-' || **format == '0' || **format == '#' || **format == '+' || **format == ' ')
+	{
+		if (**format == '-')
+			flags->minus = 1;
+		else if (**format == '0')
+			flags->zero = 1;
+		else if (**format == '#')
+			flags->hash = 1;
+		else if (**format == '+')
+			flags->plus = 1;
+		else if (**format == ' ')
+			flags->space = 1;
+		(*format)++;
+	}
+	while (**format >= '0' && **format <= '9')
+	{
+		flags->width = flags->width * 10 + (**format - '0');
+		(*format)++;
+	}
+	if (**format == '.')
+	{
+		flags->has_precision = 1;
+		(*format)++;
+	}
+	while (**format >= '0' && **format <= '9')
+	{
+		flags->precision = flags->precision * 10 + (**format - '0');
+		(*format)++;
+	}
 	/*
 	** TODO:
 	** - reset every field of `flags` to 0 first
@@ -13,7 +50,5 @@ int	ft_parse_flags(const char **format, t_flags *flags)
 	** - leave `*format` pointing AT the conversion character
 	**   ('c', 's', 'd', ...), not past it
 	*/
-	(void)format;
-	(void)flags;
-	return (0);
+	return (1);
 }
